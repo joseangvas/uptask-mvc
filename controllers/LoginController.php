0e3fac0c0361ca105baf1f387f08de1c4749d34a
Joseangvas <developer.javp@gmail.com>
2023-09-30 20:41:58 -0400

    Crear Endpoints de las Rutas en el Index público.

---
+++ controllers/LoginController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController {
+  public static function login(Router $router) {
+ 
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+    }
+
+    // Render a la Vista
+    $router->render('auth/login', [
+      'titulo' => 'Iniciar Sesión'
+    ]);
+  }
+
+
+  public static function logout() {
+    echo "Desde Logout";
+
+
+  }
+
+
+  public static function crear(Router $router) {
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+
+    // Render a la Vista
+    $router->render('auth/crear', [
+      'titulo' => 'Crear Cuenta'
+    ]);
+  }
+
+
+  public static function olvide() {
+    echo "Desde Olvide";
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+  }
+
+
+  public static function reestablecer() {
+    echo "Desde Restablecer";
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+  }
+
+
+  public static function mensaje() {
+    echo "Desde Mensaje";
+
+
+  }
+
+
+  public static function confirmar() {
+    echo "Desde Confirmar";
+
+
+  }
+}
--- public/index.php
+++ public/index.php
@@ -2,10 +2,30 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
+//* Login
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
 
+//* Crear Cuenta
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
+
+//* Formulario de Olvidé mi Password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+
+//* Colocar el Nuevo Password
+$router->get('/reestablecer', [LoginController::class, 'reestablecer']);
+$router->post('/reestablecer', [LoginController::class, 'reestablecer']);
+
+//* Confirmación de Cuenta
+$router->get('/mensaje', [LoginController::class, 'mensaje']);
+$router->get('/confirmar', [LoginController::class, 'confirmar']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
