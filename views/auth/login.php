4d0e908dcde5fbe73fb0eb61790eaea2438b976c
Joseangvas <developer.javp@gmail.com>
2023-10-02 14:46:09 -0400

    Crear archivos de estilo SASS para formulario Login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,4 +1,38 @@
 <div class="contenedor">
   <h1>UpTask</h1>
-  <p>Crea y Administra tus Proyectos</p>
+  <p class="tagline">Crea y Administra tus Proyectos</p>
+
+  <div class="contenedor-sm">
+    <p class="descripcion-pagina">Iniciar Sesión</p>
+
+    <form action="/" class="formulario" method=""POST>
+      <div class="campo">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Tu Email"
+          name="email"
+        >
+      </div>
+
+      <div class="campo">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Tu Password"
+          name="password"
+        >
+      </div>
+
+      <input type="submit" class="boton" value="Iniciar Sesión">
+    </form>
+
+    <div class="acciones">
+      <a href="/crear">¿Aún No Tienes una Cuenta? Obtener Una</a>
+      <a href="/olvide">¿Olvidaste tu Password?</a>
+    </div>
+  </div> <!-- Contenedor-sm   -->
 </div>
+
