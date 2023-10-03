107b6e11d9da4b1bca0ef621faa422b40f4dd3bc
Joseangvas <developer.javp@gmail.com>
2023-10-03 16:44:37 -0400

    Crear modulo de Crear Cuenta de Usuario

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,56 @@
-<h1>Crea tu Cuenta</h1>
+<div class="contenedor crear">
+  <?php include_once __DIR__ . '/../templates/nombre-sitio.php'; ?>
+
+  <div class="contenedor-sm">
+    <p class="descripcion-pagina">Crea tu Cuenta en UpTask</p>
+
+    <form action="/" class="formulario" method=""POST>
+      <div class="campo">
+        <label for="nombre">Nombre</label>
+        <input
+          type="text"
+          id="nombre"
+          placeholder="Tu Nombre"
+          name="nombre"
+        >
+      </div>
+
+      <div class="campo">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Tu Email"
+          name="email"
+        >
+      </div>
+
+      <div class="campo">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Tu Password"
+          name="password"
+        >
+      </div>
+
+      <div class="campo">
+        <label for="password2">Repetir Password</label>
+        <input
+          type="password"
+          id="password2"
+          placeholder="Repite tu Password"
+          name="password2"
+        >
+      </div>
+
+      <input type="submit" class="boton" value="Crear Cuenta">
+    </form>
+
+    <div class="acciones">
+      <a href="/">¿Ya Tienes una Cuenta? Iniciar Sesión</a>
+      <a href="/olvide">¿Olvidaste tu Password?</a>
+    </div>
+  </div> <!-- Contenedor-sm   -->
+</div>
