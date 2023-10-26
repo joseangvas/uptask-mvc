4ae4384f31cc6ac56f536a7b8f7679dbf054bbc8
Joseangvas <developer.javp@gmail.com>
2023-10-25 21:54:47 -0400

    Codificar Virtual DOM para listado de Tareas.

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -51,7 +51,8 @@ class TareaController {
       $respuesta = [
         'tipo' => 'exito',
         'id' => $resultado['id'],
-        'mensaje' => 'Tarea Creada Correctamente'
+        'mensaje' => 'Tarea Creada Correctamente',
+        'proyectoId' => $proyecto->id
       ];
 
       echo json_encode($respuesta);
